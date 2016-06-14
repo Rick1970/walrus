8a02bae0f6a93ab1067d0c5cd40eeb88fbfdc265
Rick Thornbrugh and russ vetsper <rick+russ@>
2016-06-14 13:26:23 -0700

    added more interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,42 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $("#clickable2").click(function() {
+  $(".clickable").click(function() {
     $("#text-showing").toggle();
     $("#text-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+    $("body").removeClass();
+    $("body").addClass("red-background");
+   });
+
+   $("button#black").click(function() {
+     $("body").addClass("black-background");
+     $("body").removeClass();
+     $("body").addClass("black-background");
+     $("body").addClass("white-text");
+    });
+
+    $("button#white").click(function() {
+      $("body").addClass("white-background");
+      $("body").removeClass();
+      $("body").addClass("white-background");
+      $("body").addClass("black-text");
+     });
+  });
